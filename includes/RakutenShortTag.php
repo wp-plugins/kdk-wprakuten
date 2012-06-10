0611e6e166c2faa2651f09ab670c64919afa1867
johna1203 <johna1203@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-10 18:53:38 +0000

    初めてのコミットです。
    git-svn-id: https://plugins.svn.wordpress.org/kdk-wprakuten/trunk@555745 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/RakutenShortTag.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Class Rakuten short tags
+ */
+class RakutenShortTag {
+	//コードの登録
+	static function register_rakuten_shorttag () {
+		add_shortcode('rakuten', 'RakutenShortTag::get_tag'); //[rakuten ]コード登録
+	}
+
+	static function get_tag ($raw_args, $content=null) {
+
+		$defaults = array (
+			'itemcode' => '',
+			'isbn' => ''
+		);
+
+		$spanitized_args = shortcode_atts($defaults, $raw_args); //コードの属性を取得
+		if (empty($spanitized_args['itemcode']) && empty($spanitized_args['isbn'])) {
+			return '';
+		}
+
+		//javascriptの登録！
+		$src = plugins_url() . '/' . KDK_WP_RAKUTEN_NAME . '/js/kdk_rakuten.jquery.js.php';
+		wp_register_script(__CLASS__, $src);
+		wp_enqueue_script('jquery');
+		wp_enqueue_script(__CLASS__);
+
+		$param['developerId'] = "feaeec38b8bc37411b14de274b1d9480";
+		$param['affiliateId'] = "0a30aaaf.afb9b1e2.0a30aab0.82880378";
+		if(!empty($spanitized_args['itemcode'])) {
+			$param['operation'] = "ItemCodeSearch";
+			$param['version'] = "2010-08-05";
+			$param['itemCode'] = $spanitized_args['itemcode'];
+		} else {
+			$param['operation'] = "BooksBookSearch";
+			$param['version'] = "2011-12-01";
+			$param['isbn'] = $spanitized_args['isbn'];
+		}
+
+		$data['itemcode'] = json_encode($param);
+		$tpl = file_get_contents(dirname(dirname(__FILE__)) . '/tpls/short_code.tpl'); //テンプレートを読み込む
+		return RakutenShortTag::parse($tpl, $data); //テンプレートのパース
+	}
+
+	static function parse ($tpl, $hash) {
+		foreach ($hash as $key => $value) {
+			$tpl = str_replace('[+'.$key.'+]', $value, $tpl);
+		}
+		return $tpl;
+	}
+
+}
+?>
---
+++ tpls/kdk_rakuten.js.tpl
@@ -0,0 +1,45 @@
+jQuery.fn.kdk_rakuten = function (param){
+	return this.each(function (){
+		var $DEVELOPER_ID = "feaeec38b8bc37411b14de274b1d9480";
+		var $AFFILIATE_ID = "0a30aaaf.afb9b1e2.0a30aab0.82880378";
+		var $API_BASE_URL = "http://api.rakuten.co.jp/rws/3.0/json";
+
+		var self = this;
+		var param = JSON.parse(jQuery(this).text().trim());
+		jQuery(self).html('loading.....');
+		jQuery.ajax({
+		  url : $API_BASE_URL,
+		  data : param,
+		  dataType : 'jsonp',
+		  jsonp : 'callBack',
+		  complete : function(json){
+		  },
+		  success : function(data, status){
+			  jQuery(self).empty();
+			  if (status == "success") {
+				  if (data.Header != undefined && data.Header.Status.toLowerCase() == "success") {
+
+					  var Item = (data.Body.ItemCodeSearch ? data.Body.ItemCodeSearch.Items.Item[0] : data.Body.BooksBookSearch.Items.Item[0]);
+
+					  var name = '';
+					  if (Item)
+					    name = Item.itemName || Item.title;
+
+					  var affiliateUrl_a = jQuery('<a></a>').attr({'href':Item.affiliateUrl,'title':name});
+					  var item_img = jQuery('<img />').attr({'src':Item.mediumImageUrl,'alt':name});
+					  affiliateUrl_a.append(item_img);
+
+					  jQuery(self).html(affiliateUrl_a);
+				  }
+			  }
+		  },
+		  beforeSend : function(){
+		  },
+		  error : function(){
+		  }
+		});
+
+	});
+}
+
+jQuery('.kdk_rakuten').kdk_rakuten();
